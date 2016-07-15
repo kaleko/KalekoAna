bad42451e3bba1acbc01dcb774e47d0858a7f945
David Kaleko <kaleko@nevis.columbia.edu>
2016-07-15 15:53:43 -0500

    run script for track data/mc that uses XiaoEventAna as an upstream filter to only consider events in which a neutrino was reconstructed

---
+++ TrackStudy/mac/run_TrackDataMCComp_XiaoNuFilter.py
@@ -0,0 +1,57 @@
+import sys
+
+if len(sys.argv) < 3:
+    msg  = '\n'
+    msg += "Usage 1: %s mc_1_data_0 $INPUT_ROOT_FILE\n" % sys.argv[0]
+    msg += '\n'
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+from ROOT import gSystem
+from ROOT import larlite as fmwk
+
+# Create ana_processor instance
+my_proc = fmwk.ana_processor()
+my_proc.enable_filter(True)
+# Set input root files
+for x in xrange(len(sys.argv)):
+    if x < 2: continue
+    my_proc.add_input_file(sys.argv[x])
+
+# Specify IO mode
+my_proc.set_io_mode(fmwk.storage_manager.kREAD)
+
+mc_1_data_0 = int(sys.argv[1])
+
+# Specify output root file name
+my_proc.set_ana_output_file("TrackDataMCComp_XiaoEvts_MC=%d_ana_out.root"%mc_1_data_0)
+
+my_running_on_data = False
+
+# Use XiaoEventAna as an event filter (returns false if no neutrino found in event)
+myxiao = fmwk.XiaoEventAna()
+myxiao.setRunningOnData(my_running_on_data)
+myxiao.setVtxSphereRadius(4.0)
+myxiao.setInputType(fmwk.kBNBCosmic)#fmwk.kBNBCosmic fmwk.kCorsikaInTime
+my_proc.add_process(myxiao)
+
+# Attach Track Data MC Comp Module
+mymod = fmwk.TrackDataMCComp()
+mymod.setTrackProducer("pandoraNuPMA")
+mymod.setRunningOnData(my_running_on_data)
+my_proc.add_process(mymod)
+
+print
+print  "Finished configuring ana_processor. Start event loop!"
+print
+
+# Let's run it.
+my_proc.run()
+
+# done!
+print
+print "Finished running ana_processor event loop!"
+print
+
+sys.exit(0)
+
