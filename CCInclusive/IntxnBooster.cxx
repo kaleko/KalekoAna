02e8dc4d0594a9656458948faa4e8e0a28032355
David Kaleko <kaleko@nevis.columbia.edu>
2016-08-05 15:24:59 -0500

    starting some code that adds tracks/etc to an already built interaction

---
+++ CCInclusive/IntxnBooster.cxx
@@ -0,0 +1,11 @@
+#ifndef LARLITE_INTXNBOOSTER_CXX
+#define LARLITE_INTXNBOOSTER_CXX
+
+#include "IntxnBooster.h"
+
+namespace larlite {
+
+
+}//end namespace larlite
+
+#endif
---
+++ CCInclusive/IntxnBooster.h
@@ -0,0 +1,51 @@
+/**
+ * \file IntxnBooster.h
+ *
+ * \ingroup CCInclusive
+ *
+ * \brief Takes in an already created neutrino interaction, builds upon it (adds more tracks, etc)
+ *
+ * @author davidkaleko
+ */
+
+/** \addtogroup CCInclusive
+
+    @{*/
+#ifndef LARLITE_INTXNBOOSTER_H
+#define LARLITE_INTXNBOOSTER_H
+
+#include <iostream>
+#include "Analysis/ana_base.h"
+#include "DataFormat/track.h"
+#include "DataFormat/vertex.h"
+#include "DataFormat/calorimetry.h"
+#include "KalekoNuItxn.h"
+#include "GeoAlgo/GeoVector.h"
+#include "GeoAlgo/GeoSphere.h"
+#include "GeoAlgo/GeoAABox.h"
+#include "CCInclusiveConstants.h"
+#include "KalekoPIDFiller.h"
+#include "FidVolBox.h"
+
+/**
+   \class IntxnBooster
+   User defined class IntxnBooster ... these comments are used to generate
+   doxygen documentation!
+ */
+namespace larlite {
+	class IntxnBooster : public larlite_base {
+
+	public:
+
+		/// Default constructor
+		IntxnBooster() {}
+
+		/// Default destructor
+		~IntxnBooster() {}
+
+	};
+
+}// end namespace larlite
+#endif
+/** @} */ // end of doxygen group
+
