a3816126f728e7b3f365e7eb0df33db9a7030e23
David Kaleko <kaleko@nevis.columbia.edu>
2016-11-18 10:34:10 -0600

    removed a lot of commented out junk code from MCSBiasStudy

--- CCInclusive/MCSBiasStudy.h
+++ CCInclusive/MCSBiasStudy.h
@@ -30,7 +30,7 @@
 
 /**
    \class MCSBiasStudy
-   A class that analyzes contained tracks in data and MC and fills a ttree with MCS info for calibration purposes.
+   A class that analyzes contained tracks in data and MC and fills a ttree with MCS info for calibration/analysis purposes.
  */
 namespace larlite {
 
@@ -41,10 +41,12 @@ namespace larlite {
         /// Default constructor
         MCSBiasStudy();
 
+        // See .cxx file for descriptions of each of these types
         enum AnalysisType_t {
-            kSingleMuonMCTrack,
+            kSingleMuonMCTrack, 
             kSingleMuonRecoTrack,
             kMCBNBSelectedRecoTrack,
+            kMCBNBRecoTrack,
             kDataBNBSelectedRecoTrack,
             kANALYSIS_TYPE_MAX
         };
@@ -64,10 +66,10 @@ namespace larlite {
         TTree* GetTMCTree() { return _tmc->GetTree(); }
 
         /// Function to compute the average angle deviation for custom-segmented tracks
-        std::pair<double, double> ComputeWiggle(const larlite::track &track, double seg_size);
+        // std::pair<double, double> ComputeWiggle(const larlite::track &track, double seg_size);
 
         /// Takes the dot product between the first 50cm of the track direction and the start-to-end direction
-        double ComputeLongCurve(const larlite::track &track);
+        // double ComputeLongCurve(const larlite::track &track);
 
         void SetAnalysisType(AnalysisType_t mytype) { _ana_type = mytype; }
 
@@ -79,54 +81,19 @@ namespace larlite {
 
         TrackMomentumSplines _myspline;
         kaleko::TrackMomentumCalculator *_tmc;
-        TrackChopper _chopper;
-        TrackSmearer _smearer;
-        ::geoalgo::GeoAlgo _geoalg;
+        // ::geoalgo::GeoAlgo _geoalg;
 
         TTree *_tree;
-        double _length_analyzed;
+        // double _length_analyzed;
         double _full_length;
-        double _chopped_full_length;
-        double _MCS_energy;
         double _full_MCS_energy;
-        double _full_MCS_energy_reweighted;
-        double _full_MCS_energy_someflipped;
-        double _full_MCS_energy_chopped;
-        double _full_MCS_energy_smeared;
-        double _full_MCS_energy_chopped_smeared;
+        double _full_MCS_momentum;
         double _full_range_energy;
-        double _track_start_x;
-        double _track_start_y;
-        double _track_start_z;
-        double _track_end_x;
-        double _track_end_y;
-        double _track_end_z;
-        double _track_dot_z;
-        int _n_traj_points;
-        double _chopped_wiggle;
-        double _chopped_std;
-        double _chopped_smeared_wiggle;
-        double _chopped_smeared_std;
-        double _smeared_wiggle;
-        double _smeared_std;
-        double _long_curve_dotprod;
-        bool _full_track_tree_entry;
+        double _full_range_momentum;
         double _true_E;
-
         int _run;
         int _subrun;
         int _eventid;
-
-        TTree *_seg_tree;
-        double _seg_start_x;
-        double _seg_start_y;
-        double _seg_start_z;
-        double _seg_end_x;
-        double _seg_end_y;
-        double _seg_end_z;
-        int    _n_seg_traj_points;
-        double _seg_avg_perp_dist;
-        double _seg_std_perp_dist;
 
     };
 
